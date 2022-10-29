38999baac047e6a5df881d959b661113931dadf7
Shipan <liushipan1998@gmail.com>
2022-10-29 21:45:42 +0200

    28.10.2022

---
+++ 02_Vue_examples/data/data.js
@@ -0,0 +1,37 @@
+const books = [
+  {
+    id: 1,
+    name: '《book1》',
+    date: '2006-9',
+    price: 85.00,
+    count: 1
+  },
+  {
+    id: 2,
+    name: '《book2》',
+    date: '2006-2',
+    price: 59.00,
+    count: 1
+  },
+  {
+    id: 3,
+    name: '《book3》',
+    date: '2008-10',
+    price: 39.00,
+    count: 1
+  },
+  {
+    id: 4,
+    name: '《book4》',
+    date: '2006-3',
+    price: 128.00,
+    count: 1
+  },
+  {
+    id: 5,
+    name: '《book5》',
+    date: '2014-8',
+    price: 88.00,
+    count: 1
+  },
+]
