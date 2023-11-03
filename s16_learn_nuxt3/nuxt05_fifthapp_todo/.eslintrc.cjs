cf83632578ae16d48c7cee66d850f9f5e6c3f10f
Shipan <liushipan1998@gmail.com>
2023-11-03 14:44:13 +0100

    start todo app

---
+++ s16_learn_nuxt3/nuxt05_fifthapp_todo/.eslintrc.cjs
@@ -0,0 +1,36 @@
+// .eslintrc.cjs
+// By default, if your package.json contains "type": "module", then Node.js will treat all .js files
+// as ESM (ECMAScript Modules). But ESLint, as of now, expects its config files to be in CommonJS format.
+// This is why the .cjs extension is often recommended.
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser", // Only needed if you are using TypeScript
+  },
+  extends: [
+    "@nuxtjs/eslint-config-typescript", // Only needed if you are using TypeScript
+  ],
+  plugins: [],
+
+  rules: {
+    "no-multiple-empty-lines": ["error", { max: 9999, maxEOF: 100 }],
+    quotes: ["error", "double",], // 之所以 key 是 unquoted, 是 quotes is a single word, Quoted keys are necessary when the key contains special characters, spaces, or reserved words.
+    "comma-dangle": "off", // allow a , after the last item in an object
+    semi: "off", // 最后 加不加 ; 是无所谓的
+    "padded-blocks": "off", // function body 里面允许有空行
+    // "no-trailing-spaces": "off", // fucntion body 每一行前面允许有 空行。
+    "keyword-spacing": ["error", { overrides: { if: { after: false } } }], // if 和 后面的 () 之间不用有 space， 但是 eslint 是要求有的。
+    "arrow-parens": "off", // a single argument of an arror function can also have a ()
+    "vue/v-on-event-hyphenation": "off", // 可以使用 驼峰naming, not necessarily hyphen
+    "no-console": "off", // because using console.log statements is generally discouraged in production code, as it could potentially leak sensitive information to the console,我这里允许使用console without getting a warning
+    "vue/singleline-html-element-content-newline": "off", // 可以使用 <p>x</p> 不用x单独在一行
+    "space-before-function-paren": "off", // fun()  or  fun ()
+    "vue/html-self-closing": "off", //  <span class="slider round"></span> 允许， 不一定 非要 <span class="slider round" />
+  },
+};
---
+++ s16_learn_nuxt3/nuxt05_fifthapp_todo/nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// 更改过了 config file， 要重新 npm run dev
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: ["@pinia/nuxt"],
+
+  // 参考：https://tailwindcss.com/docs/guides/nuxtjs
+  css: ["~/assets/css/main.css"],
+
+  // 参考：https://tailwindcss.com/docs/guides/nuxtjs
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  // runtime config: To expose config and environment variables to the rest of your app, you will need to define runtime configuration in your nuxt.config file, using the runtimeConfig option.
+  runtimeConfig: {
+    // The private keys which are ONLY available within server-side
+    exampleApiSecret: "this is only seeable in server side", // 可以用 .env内部的内容
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      appid: process.env.WEATHER_APP_SECRET // 调用.env内部的内容
+    }
+  }
+
+  // 我想要使用 botstrap：可以用npm，也可以使用 CDN
+  // app: {
+  //   head: {
+  //     link: [
+  //       { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" }
+  //     ]
+  //   }
+  // }
+})
