e0de3453648fe1f008dd1188e8d4f3ec4d6e954c
shipan_macmini <liushipan1998@gmail.com>
2023-11-07 18:41:49 +0100

    Add: Add sidebar and headerbar

--- s17_BA/p1_echarts_qianfeng/components/sidebar/sidebar.vue
+++ s17_BA/p1_echarts_qianfeng/components/sidebar/sidebar.vue
@@ -1,12 +1,28 @@
-<script setup lang="ts">
+<script setup>
 
+const links = [{
+  label: "Profile",
+  avatar: {
+    src: "https://avatars.githubusercontent.com/u/739984?v=4"
+  },
+  badge: 100
+}, {
+  label: "Installation",
+  icon: "i-heroicons-home",
+  to: "#"
+}, {
+  label: "Vertical Navigation",
+  icon: "i-heroicons-chart-bar",
+  to: "#"
+}, {
+  label: "Command Palette",
+  icon: "i-heroicons-command-line",
+  to: "#"
+}]
 </script>
 
-
 <template>
-  <div>
-    sidebar
-  </div>
+  <UVerticalNavigation :links="links" />
 </template>
 
 
--- s17_BA/p1_echarts_qianfeng/layouts/default.vue
+++ s17_BA/p1_echarts_qianfeng/layouts/default.vue
@@ -2,6 +2,7 @@
 
 <script setup lang="ts">
 import Headbar from "@/components/headbar/headerbar.vue"
+import Sidebar from "@/components/sidebar/sidebar.vue"
 
 
 
@@ -10,9 +11,9 @@ import Headbar from "@/components/headbar/headerbar.vue"
 
 
 <template>
-  <div id="main-layout" class="flex flex-col w-screen h-screen bg-gray-100">
+  <div id="main-layout" class="flex flex-col w-screen h-screen">
     <!-- Header area -->
-    <header class="text-white w-full h-16 flex items-center px-4">
+    <header class="text-white w-full h-16 flex items-center px-4 flxe-row-reverse">
       <!-- Navigation content goes here -->
       <Headbar />
     </header>
@@ -22,10 +23,11 @@ import Headbar from "@/components/headbar/headerbar.vue"
       <!-- Sidebar area -->
       <aside class=" w-1/12  h-full overflow-y-auto">
         <!-- Sidebar content goes here -->
+        <Sidebar />
       </aside>
 
       <!-- Main content area -->
-      <main class="flex-1 bg-white overflow-y-auto p-4">
+      <main class="flex-1 bg-gray-100 overflow-y-auto p-4">
         <slot />
       </main>
     </div>
